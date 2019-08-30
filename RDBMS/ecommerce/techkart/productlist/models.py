40fa5b4f0e20926d69051d88e6354db8ad293a09
Sehaj Chawla <sehaj23rocks@gmail.com>
2019-08-30 12:08:32 +0530

    accessroies completed

--- RDBMS/ecommerce/techkart/productlist/models.py
+++ RDBMS/ecommerce/techkart/productlist/models.py
@@ -29,5 +29,21 @@ class productreviews(models.Model):
     class meta:
         db_table = "Productreviews"
 
+class hotdeals(models.Model):
+    id = models.IntegerField(max_length=10, primary_key=True)
+    p_name = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
+    quantity = models.IntegerField(max_length=10)
+    price = models.IntegerField(max_length=100)
+    oldprice = models.IntegerField(max_length=200)
+    description = models.TextField(max_length=200)
+    p_specification = models.TextField(max_length=600)
+    p_color = models.CharField(max_length=100)
+    mainphoto = models.ImageField(upload_to="photos/%Y/%m/%d/")
+    photo1 = models.ImageField(upload_to="photos/%Y/%m/%d/", blank=True)
+    photo2 = models.ImageField(upload_to="photos/%Y/%m/%d/", blank=True)
+    def __str__(self):
+       return self.p_name
+
 
 
